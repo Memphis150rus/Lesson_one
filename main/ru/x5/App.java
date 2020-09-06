d4e355177a71e2ca019a4371aa3e7be93fafbcbd
Andrew <A@>
2020-09-06 21:26:59 +0300

    New add directory

--- main/ru/x5/App.java
+++ main/ru/x5/App.java
@@ -1,7 +1,11 @@
 package ru.x5;
 
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello X5 people!");
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        System.out.println(value);
     }
 }
